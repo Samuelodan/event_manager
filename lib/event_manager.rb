aaa21043e34046e0d65e8af3b93dac6bb0777258
Samuel ODaniels <samodan195@gmail.com>
2022-05-25 15:46:47 +0100

    Refactor #clean_zipcode body into one line

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,15 +5,7 @@ require 'csv'
 puts 'Event Manager Initialized'
 
 def clean_zipcode(zip)
-  if zip.nil?
-    '00000'
-  elsif zip.length < 5
-    zip.rjust(5, '0')
-  elsif zip.length > 5
-    zip[0...5]
-  else
-    zip
-  end
+  zip.to_s.rjust(5, '0')[0...5]
 end
 
 contents = CSV.open(
