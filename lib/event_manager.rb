d20040f0534f6f777c9160dfc88a34bf4853471e
Samuel ODaniels <samodan195@gmail.com>
2022-05-25 22:30:51 +0100

    Extract legislator code into new #legislators_by_zip method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -9,19 +9,9 @@ def clean_zipcode(zip)
   zip.to_s.rjust(5, '0')[0...5]
 end
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
-
-contents.each do |row|
-  name = row[:first_name]
-  zip = clean_zipcode(row[:zipcode])
-
+def legislators_by_zip(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
   begin
     legislators = civic_info.representative_info_by_address(
       address: zip,
@@ -32,5 +22,19 @@ contents.each do |row|
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
+  legislators
+end
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zip = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zip(zip)
+
   puts "#{name}\t#{zip}\t#{legislators}"
 end
