ce24ae3061fa7a31dafae9aee416396de89c86ef
Samuel ODaniels <samodan195@gmail.com>
2022-05-25 15:16:27 +0100

    Add code in each block to deal with invalid zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -14,5 +14,13 @@ contents.each do |row|
   name = row[:first_name]
   zip = row[:zipcode]
 
+  if zip.nil?
+    zip = '00000'
+  elsif zip.length < 5
+    zip = zip.rjust(5, '0')
+  elsif zip.length > 5
+    zip = zip[0...5]
+  end
+
   puts "#{name}\t#{zip}"
 end
